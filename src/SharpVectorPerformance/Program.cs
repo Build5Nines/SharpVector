b14235140c70aa37f0e891499c35f8017076deee
Chris Pietschmann <chris@build5nines.com>
2025-02-22 14:04:23 -0500

    added BenchmarkDotNet tests for save/load vector database

---
+++ src/SharpVectorPerformance/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+
+namespace SharpVectorPerformance;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<MemoryVectorDatabasePerformance>();
+    }
+}
