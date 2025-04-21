c50bf27d6791107507a9060257909964054acfaf
Chris Pietschmann <chris@build5nines.com>
2025-04-21 10:31:53 -0400

    add tests

--- src/SharpVectorTest/VectorStore/MemoryDictionaryVectorStoreTest.cs
+++ src/SharpVectorTest/VectorStore/MemoryDictionaryVectorStoreTest.cs
@@ -47,4 +47,34 @@ public class MemoryDictionaryVectorStoreTests
         Assert.AreEqual(11.0, vectorStoreTwo.Get(4).Vector[1]);
         Assert.AreEqual(12.0, vectorStoreTwo.Get(4).Vector[2]);
     }
+
+    [TestMethod]
+    public void MemoryVectorStore_001()
+    {
+        var vectorStore = new MemoryDictionaryVectorStore<int, string>();
+        vectorStore.Set(1, new VectorTextItem<string, string>("key1", "1", new float[] { 1.0F, 2.0F, 3.0F }));
+        vectorStore.Set(2, new VectorTextItem<string, string>("key2", "2", new float[] { 4.0F, 5.0F, 6.0F }));
+        vectorStore.Set(3, new VectorTextItem<string, string>("key3", "3", new float[] { 7.0F, 8.0F, 9.0F }));
+        vectorStore.Set(4, new VectorTextItem<string, string>("key4", "4", new float[] { 10.0F, 11.0F, 12.0F }));
+
+        var item = vectorStore.Get(2);
+        Assert.AreEqual("key2", item.Text);
+    }
+
+    [TestMethod]
+    public void MemoryVectorStore_002()
+    {
+        var vectorStore = new MemoryDictionaryVectorStore<int, string>();
+        vectorStore.Set(1, new VectorTextItem<string, string>("key1", "1", new float[] { 1.0F, 2.0F, 3.0F }));
+        vectorStore.Set(2, new VectorTextItem<string, string>("key2", "2", new float[] { 4.0F, 5.0F, 6.0F }));
+        vectorStore.Set(3, new VectorTextItem<string, string>("key3", "3", new float[] { 7.0F, 8.0F, 9.0F }));
+        vectorStore.Set(4, new VectorTextItem<string, string>("key4", "4", new float[] { 10.0F, 11.0F, 12.0F }));
+
+        foreach(var item in vectorStore)
+        {
+            Assert.IsNotNull(item.Value);
+            Assert.AreNotEqual(0, item.Key);
+        }
+    }
+
 }
