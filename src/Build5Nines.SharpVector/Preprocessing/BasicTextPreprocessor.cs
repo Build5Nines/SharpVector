80babed335564e6878515310e972ecb98f2903b1
Chris Pietschmann <chris@build5nines.com>
2025-04-21 13:28:21 -0400

    Clean up BasicTextPreprocessor code a bit

--- src/Build5Nines.SharpVector/Preprocessing/BasicTextPreprocessor.cs
+++ src/Build5Nines.SharpVector/Preprocessing/BasicTextPreprocessor.cs
@@ -1,8 +1,6 @@
 namespace Build5Nines.SharpVector.Preprocessing;
 
 using System.Globalization;
-using System.Security.AccessControl;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -13,7 +11,7 @@ public class BasicTextPreprocessor : ITextPreprocessor<string>
 
     private const string regexChineseCharactersPattern = @"\p{IsCJKUnifiedIdeographs}";
     private const string regexRemovePunctuation = @"[\p{P}$^`~=+|<>]"; // @"[\p{P}]";
-    private const string regexTokenize = @"[\p{IsCJKUnifiedIdeographs}]|\p{So}\p{Sk}|[a-z0-9]+";
+    // private const string regexTokenize = @"[\p{IsCJKUnifiedIdeographs}]|\p{So}\p{Sk}|[a-z0-9]+";
     private const string regexWhitespacePattern = @"\s+";
     private const string regexEmojiPattern = @"[\p{So}\uD83C-\uDBFF\uDC00-\uDFFF]";    
 
@@ -21,45 +19,53 @@ public class BasicTextPreprocessor : ITextPreprocessor<string>
     {
         if (string.IsNullOrWhiteSpace(text)) return Array.Empty<string>();
 
+        // Tokens should always be lower case
         text = text.ToLower();
 
         // Remove punctuation (excluding Chinese characters)
         text = Regex.Replace(text, regexRemovePunctuation, string.Empty);
-        //text = Regex.Replace(text, @"[$^`~=+|<>]", space);
 
-        Console.WriteLine($"Text after removing punctuation: {text}");
+        // Space pad special characters (Emoji and Chinese characters)
+        text = SpacePadSpecialCharacters(text);
 
-        // Check if text contains Chinese characters using the CJK Unified Ideographs block
-        if (Regex.IsMatch(text, regexChineseCharactersPattern))
-        {
-            if (Regex.IsMatch(text, regexEmojiPattern))
-            {
-                // Has Emoji
-                text = SpacePadSpecialCharacters(text, new string[] { regexEmojiPattern, regexChineseCharactersPattern });
-                // remove extra whitespace characters
-                text = Regex.Replace(text, regexWhitespacePattern, space).Trim();
-            } else {
-                // No Emoji
-                // Tokenize either by matching individual Chinese characters or contiguous word tokens (for Latin letters/digits)
-                var tokens = Regex.Matches(text, regexTokenize)
-                                .Cast<Match>()
-                                .Select(m => m.Value);
-                return tokens;
-            }
-        }
-        else
-        {
-            // if text contains emojis
-            if (Regex.IsMatch(text, regexEmojiPattern))
-            {
-                text = SpacePadSpecialCharacters(text, new string[] { regexEmojiPattern });
-            }
-            
-            // remove extra whitespace characters
-            text = Regex.Replace(text, regexWhitespacePattern, space).Trim();   
-        }
+        // Remove extra whitespace characters
+        text = Regex.Replace(text, regexWhitespacePattern, space).Trim();   
 
+        // Split to Token array
         return text.Split(charSpace);
+
+
+        // // Check if text contains Chinese characters using the CJK Unified Ideographs block
+        // if (Regex.IsMatch(text, regexChineseCharactersPattern))
+        // {
+        //     if (Regex.IsMatch(text, regexEmojiPattern))
+        //     {
+        //         // Has Emoji
+        //         text = SpacePadSpecialCharacters(text, new string[] { regexEmojiPattern, regexChineseCharactersPattern });
+        //         // remove extra whitespace characters
+        //         text = Regex.Replace(text, regexWhitespacePattern, space).Trim();
+        //     } else {
+        //         // No Emoji
+        //         // Tokenize either by matching individual Chinese characters or contiguous word tokens (for Latin letters/digits)
+        //         var tokens = Regex.Matches(text, regexTokenize)
+        //                         .Cast<Match>()
+        //                         .Select(m => m.Value);
+        //         return tokens;
+        //     }
+        // }
+        // else
+        // {
+        //     // if text contains emojis
+        //     if (Regex.IsMatch(text, regexEmojiPattern))
+        //     {
+        //         text = SpacePadSpecialCharacters(text, new string[] { regexEmojiPattern });
+        //     }
+            
+        //     // remove extra whitespace characters
+        //     text = Regex.Replace(text, regexWhitespacePattern, space).Trim();   
+        // }
+
+        // return text.Split(charSpace);
     }
 
     public async Task<IEnumerable<string>> TokenizeAndPreprocessAsync(string text)
@@ -68,7 +74,35 @@ public class BasicTextPreprocessor : ITextPreprocessor<string>
     }
 
 
-    private static string SpacePadSpecialCharacters(string text, string[] regexPatterns){
+    private static string SpacePadSpecialCharacters(string text)
+    {
+        var spacePadPatterns = new List<string>();
+
+        // Contains Chinese characters?
+        if (Regex.IsMatch(text, regexChineseCharactersPattern))
+        {
+            // Space pad Chinese characters
+            spacePadPatterns.Add(regexChineseCharactersPattern);
+        }
+
+        // Contains Emoji?
+        if (Regex.IsMatch(text, regexEmojiPattern))
+        {
+            // Space pad Emoji characters
+            spacePadPatterns.Add(regexEmojiPattern);
+        }
+
+        if (spacePadPatterns.Count > 0)
+        {
+            // Space pad special characters based on the patterns selected
+            text = SpacePadSpecialCharacters(text, spacePadPatterns.ToArray());
+        }
+
+        return text;
+    }
+
+    private static string SpacePadSpecialCharacters(string text, string[] regexPatterns)
+    {
         var enumerator = StringInfo.GetTextElementEnumerator(text);
         StringBuilder sb = new StringBuilder();
         int i;
